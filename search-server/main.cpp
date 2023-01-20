cb0b5a79d96e90274327085128e9e5b87ec0b219
FireE1 <53971124+FireE1@users.noreply.github.com>
2023-01-20 15:52:18 +0400

    minor changes to second sprint

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -6,10 +6,12 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
+const int MAGICK_NUMBER = 1e-6;
 
 string ReadLine() {
     string s;
@@ -86,7 +88,7 @@ public:
 
         sort(matched_documents.begin(), matched_documents.end(),
              [](const Document& lhs, const Document& rhs) {
-                 if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
+                 if (abs(lhs.relevance - rhs.relevance) < MAGICK_NUMBER) {
                      return lhs.rating > rhs.rating;
                  } else {
                      return lhs.relevance > rhs.relevance;
@@ -154,10 +156,7 @@ private:
         if (ratings.empty()) {
             return 0;
         }
-        int rating_sum = 0;
-        for (const int rating : ratings) {
-            rating_sum += rating;
-        }
+        int rating_sum = accumulate(ratings.begin(), ratings.end(), 0);
         return rating_sum / static_cast<int>(ratings.size());
     }
 
