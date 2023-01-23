7e703f92fb161bdad76a0def2fe66bae9759eef1
FireE1 <53971124+FireE1@users.noreply.github.com>
2023-01-23 18:17:13 +0400

    3rd sprint 2nd changes

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -31,29 +31,10 @@ void TestOnDocumentAdd () {
     const vector<int> ratings = {1, 2, 3};
     {
     SearchServer server;
+        const auto found_docs = server.FindTopDocuments("cat"s);
+        ASSERT(found_docs.empty());
     server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
         const auto found_docs = server.FindTopDocuments("cat"s);
-        const Document& doc0 = found_docs[0];
-        ASSERT_EQUAL(doc0.id, doc_id);
-    }
-    {
-    SearchServer server;
-    server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
-        const auto found_docs = server.FindTopDocuments("in"s);
-        const Document& doc0 = found_docs[0];
-        ASSERT_EQUAL(doc0.id, doc_id);
-    }
-    {
-    SearchServer server;
-    server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
-        const auto found_docs = server.FindTopDocuments("the"s);
-        const Document& doc0 = found_docs[0];
-        ASSERT_EQUAL(doc0.id, doc_id);
-    }
-    {
-    SearchServer server;
-    server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
-        const auto found_docs = server.FindTopDocuments("city"s);
         const Document& doc0 = found_docs[0];
         ASSERT_EQUAL(doc0.id, doc_id);
     }
@@ -91,7 +72,7 @@ void TestOnStatusFilt () {
     }
 }
 
-void TestOnPredictFilt () {
+void TestOnPredicatFilt () {
     const int doc_id = 42;
     const string content = "cat in the city"s;
     const vector<int> ratings = {1, 2, 3};
@@ -153,12 +134,60 @@ void TestRatingOnDocuments () {
         ASSERT_EQUAL(found_docs.size(), 2);
         const Document& doc0 = found_docs[0];
         const Document& doc2 = found_docs[1];
-        ASSERT_EQUAL(doc0.rating, 5);
-        ASSERT_EQUAL(doc2.rating, 2);
+        ASSERT_EQUAL(doc0.rating, (accumulate(ratings_2.begin(), ratings_2.end(), 0) / static_cast<int>(ratings_2.size())));
+        ASSERT_EQUAL(doc2.rating, (accumulate(ratings.begin(), ratings.end(), 0) / static_cast<int>(ratings.size())));
     }
 }
 
+double Relevance (vector<string> content, vector<string> q, size_t docs_num) {
+    double relev = 0.0;
+    int TF_words_match = 0;
+    set<string> IDF_words;
+    for (const string& i : content)
+    {
+        for (const string& u : q)
+        {
+            if (i == u)
+            {
+                ++IDF_words_match;
+                TF_words.insert(u);
+            }
+        }
+    }
+    relev = (log((docs_num * 1.0) / (IDF_words * 1.0))) * ((TF_words_match * 1.0) / (content.size() * 1.0));
+    return relev;
+}
+
 void TestRelevanceOnDocuments () {
+    const int doc_id = 42;
+    const string content = "cat in the city"s;
+    const vector<int> ratings = {1, 2, 3};
+    
+    const int doc_id_2 = 48;
+    const string content_2 = "dog on the city"s;
+    const vector<int> ratings_2 = {4, 5, 6};
+
+    const string q = "on city"s;
+
+    const double inv_word_count = 1.0 / words.size();
+    log(GetDocumentCount() * 1.0 / word_to_document_freqs_.at(word).size());
+    SplitIntoWords()
+    {
+    SearchServer server;   
+    server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
+    server.AddDocument(doc_id_2, content_2, DocumentStatus::ACTUAL, ratings_2);
+        const auto found_docs = server.FindTopDocuments(q);
+        const Document& doc0 = found_docs[0];
+        const Document& doc2 = found_docs[1];
+        const double doc0_rel = Relevance(SplitIntoWords(content_2), SplitIntoWords(q), found_docs.size());
+        const double doc2_rel = Relevance(SplitIntoWords(content), SplitIntoWords(q), found_docs.size());
+        ASSERT_EQUAL(doc0.relevance, doc0_rel);
+        ASSERT_EQUAL(doc2.relevance, doc2_rel);
+        ASSERT(doc0.relevance > doc2.relevance);
+    }
+}
+
+void TestSortingOnDocuments () {
     const int doc_id = 42;
     const string content = "cat in the city"s;
     const vector<int> ratings = {1, 2, 3};
@@ -175,7 +204,6 @@ void TestRelevanceOnDocuments () {
         const Document& doc2 = found_docs[1];
         ASSERT_EQUAL(doc0.id, doc_id_2);
         ASSERT_EQUAL(doc2.id, doc_id);
-        ASSERT(doc0.relevance > doc2.relevance);
     }
 }
 
@@ -188,5 +216,6 @@ void TestSearchServer() {
     RUN_TEST(TestOnDocumentAdd);
     RUN_TEST(TestOnFuncMatch);
     RUN_TEST(TestOnStatusFilt);
-    RUN_TEST(TestOnPredictFilt);
+    RUN_TEST(TestOnPredicatFilt);
+    RUN_TEST(TestSortingOnDocuments);
 }
